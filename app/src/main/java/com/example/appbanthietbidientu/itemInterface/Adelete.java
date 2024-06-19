80bd40eeb20beb7ad8baa9ee8de8a21471fa21f9
tranquangvu2003 <144189279+tranquangvu2003@users.noreply.github.com>
2024-06-19 23:43:54 +0700

    remove produc delete user ... design layout

---
+++ app/src/main/java/com/example/appbanthietbidientu/itemInterface/Adelete.java
@@ -0,0 +1,14 @@
+package com.example.appbanthietbidientu.itemInterface;
+
+import com.example.appbanthietbidientu.model.Sanpham;
+import com.example.appbanthietbidientu.model.User;
+
+public abstract class Adelete implements IDelete{
+    @Override
+    public void deleteProduct(Sanpham sanpham) {
+
+    }
+    public void deleteUser(User user){
+
+    }
+}
